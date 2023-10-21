a715618ee9e7cbc2d507e0e4cc2e1b8666ca5d11
Dilshod0456 <dilshod0456@gmail.com>
2023-10-21 12:31:43 +0500

    week3

--- MySite/settings.py
+++ MySite/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'MySite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'week2/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
