78e1848f617e1d631c7cfea51d2dbec30d25a0b5
Dilshod0456 <dilshod0456@gmail.com>
2023-10-22 00:48:07 +0500

    modul2 week1

--- week2/models.py
+++ week2/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Questions(models.Model):
+    name= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Choice(models.Model):
+        questtion = models.ForeignKey(Questions,on_delete=models.CASCADE )
+        choice_text = models.CharField(max_length=3)
+        Votes = models.IntegerField(default=0)
+
+        def __str__(self):
+            return self.choice_text
+        
