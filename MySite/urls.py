3bbe000c24fcc2af632affce1502a9db1e9082d9
Dilshod0456 <dilshod0456@gmail.com>
2023-10-22 01:33:24 +0500

    modul2 week1

--- MySite/urls.py
+++ MySite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('week2', include('week2.urls'))
+    path('week2', include('week2.urls')),
+    path('polls/', include('polls.urls', namespace='polls')),
 ]
