8738082862a3d9c4409850777882205ae813b544
Dilshod0456 <dilshod0456@gmail.com>
2023-10-23 17:55:42 +0500

    m3 w5

--- cats/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from cats.models import Breed
-
-
-# Create the form class.
-class BreedForm(ModelForm):
-    class Meta:
-        model = Breed
-        fields = '__all__'
--- dj4e/settings.py
+++ dj4e/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+APP_NAME = 'ChucksList'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -25,7 +25,7 @@ SECRET_KEY = '=o(+)#v*z168p)e_(^%w_dhi^pu%vpro3&n)%#lrc=5$jjvg8!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['723160442c18.ngrok.io', '127.0.0.1']
 
 
 # Application definition
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'autos',
-    'cats',
-    "home",
+    'django.contrib.humanize',
+    'home',
+    'ads',
+    "social_django",
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'uni_form'
+# CRISPY_TEMPLATE_PACK = 'bootstrap5' 
