c8bc25dcc26a0ac6fa4f6c1f4871a5fdda514bc4
Dilshod0456 <dilshod0456@gmail.com>
2023-10-24 16:55:53 +0500

    m4 w5

--- dj4e/settings.py
+++ dj4e/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=o(+)#v*z168p)e_(^%w_dhi^pu%vpro3&n)%#lrc=5$jjvg8!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8b7eaafceb20.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['*', '127.0.0.1']
 
 
 # Application definition
